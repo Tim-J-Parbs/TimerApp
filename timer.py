00d949d3a5b758b606ab42f8838fa072424bccdd
Tim@work <t.parbs@uni-luebeck.de>
2022-07-12 20:08:48 +0200

    Added new buttons, improved tracking of slides, added slide tracking and undo functionality

--- timer.py
+++ timer.py
@@ -9,7 +9,7 @@ class Application(tk.Tk):
     def __init__(self,  master=None):
         tk.Tk.__init__(self, master)
         #self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
-        self.geometry('600x800')
+        self.geometry('700x800')
         self.resizable(True, True)
         self.title('Timer')
         self.mainframe()
@@ -19,7 +19,8 @@ class Application(tk.Tk):
         self.timer_interval = 0.01
         self.stopwatch = self.Stopwatch(self.timer_interval, self.update_time, self.timer_stopped)
         self.update_time(0)
-        self.slidecounter  = 1
+        self.slidecounter  = 0
+        self.eventtype = []
     def mainframe(self):
         # Define interface
         self.columnconfigure(6, weight=1)
@@ -33,12 +34,13 @@ class Application(tk.Tk):
         self.scrollbar = tk.Scrollbar(master=self, orient=tk.VERTICAL)
         self.eventlistbox.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.eventlistbox.yview)
-
         self.run = tk.Button(self, text="run", command=self.timer_start)
         self.stop = tk.Button(self, text="stop", command=self.timer_stop)
         self.clear = tk.Button(self, text="clear", command=self.timer_clear)
-        self.newevent_slide_done = tk.Button(self, text="Slide Done", command=self.Slide_done)
-        self.newevent_interrupt = tk.Button(self, text="Interruption", command=self.Interrupt)
+        self.newevent_slide_done = tk.Button(self, text="Folie fertig", command=self.Slide_done)
+        self.newevent_interrupt = tk.Button(self, text="Unterbrechung", command=self.Interrupt)
+        self.newevent_again = tk.Button(self, text="Folie nochmal", command=self.Slide_again)
+        self.newevent_clearlast = tk.Button(self, text="Zurück", command=self.msg_delete)
         self.export = tk.Button(self, text="Export Log", command=self.logexport)
         self.eventlistbox.grid(row=1, column=0, rowspan=20, columnspan=2,
                     sticky='nswe')
@@ -49,13 +51,16 @@ class Application(tk.Tk):
         self.stop.grid(row=1, column=5, sticky='nswe')
         self.newevent_slide_done.grid(row=2, column=4, sticky='nswe')
         self.newevent_interrupt.grid(row=2, column=5, sticky='nswe')
-        self.export.grid(row=3, column=4, sticky='nswe')
+        self.newevent_again.grid(row=2, column=6, sticky='nswe')
+        self.export.grid(row=3, column=5, sticky='nswe')
+        self.newevent_clearlast.grid(row=3, column=4, sticky='nswe')
         self.clear.grid(row=1, column=6, sticky='nswe')
         self.text_eventlist = []
         # Start main loop
-    def msg_update(self, time, msg):
-        if self.stopwatch.running:
-            msglen = 10
+    def msg_update(self, time, msg, event, override_exec = False):
+        if self.stopwatch.running or override_exec:
+            self.eventtype.append(event)
+            msglen = 20
             timestr = datetime.datetime.strftime(datetime.datetime.utcfromtimestamp(time), "%H:%M:%S")
             if (msglen-len(msg)) <0:
                 msg = msg[:msglen-2]+'...'
@@ -63,13 +68,22 @@ class Application(tk.Tk):
             tolist = timestr + ' - ' + interr + msg
             self.text_eventlist.append(tolist)
             self.eventlist.set(self.text_eventlist)
-
+    def msg_delete(self):
+        if len(self.eventtype) == 0:
+            return
+        event = self.eventtype.pop()
+        if event == 'slideup':
+            self.slidecounter -= 1
+        self.text_eventlist.pop()
+        self.eventlist.set(self.text_eventlist)
     def timer_start(self):
         if not self.stopwatch.running:
             print("Starting thread")
             self.timer_stopped.clear()
             self.timer_running.set()
-            self.stopwatch.run()
+            if len(self.text_eventlist) == 0:
+                self.Slide_done(override_exec=True)
+            self.stopwatch.run() # This blocks!
         else:
             print('Already running!')
 
@@ -93,7 +107,10 @@ class Application(tk.Tk):
             self.stoptime = time.time()-self.starttime
             self.running = False
         def get_time(self):
-            return time.time()-self.starttime
+            if self.running:
+                return time.time()-self.starttime
+            else:
+                return self.stoptime - self.starttime
         def update(self):
             self.fn(time.time()-self.starttime)
         def clear(self):
@@ -122,15 +139,17 @@ class Application(tk.Tk):
 
     def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
-    def Slide_done(self):
-        if self.stopwatch.running:
+    def Slide_done(self, override_exec=False):
+        if self.stopwatch.running or override_exec:
             self.slidecounter  +=1
             msg = 'Folie ' + str(self.slidecounter)
-            self.msg_update(self.stopwatch.get_time(),msg)
-
+            self.msg_update(self.stopwatch.get_time(),msg, 'slideup',override_exec=override_exec)
     def Interrupt(self):
-        msg = 'Störung'
-        self.msg_update(self.stopwatch.get_time(), msg)
+        msg = 'Unterbrechung'
+        self.msg_update(self.stopwatch.get_time(), msg, 'interrupt')
+    def Slide_again(self):
+        msg = 'Wdh: Folie ' + str(self.slidecounter)
+        self.msg_update(self.stopwatch.get_time(), msg, 'slideagain')
     def logexport(self):
         timestr = datetime.datetime.strftime(datetime.datetime.utcfromtimestamp(time.time()), "%M:%D:%H:%M:%S:%f")
         with open('./run.txt', 'w+')as f:
